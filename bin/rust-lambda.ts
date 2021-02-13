cc5ae56d095ca43ceed949545c31b50e4052f5dc
Paweł Motyl <pawmot@gmail.com>
2021-02-13 18:55:47 +0100

    Initial commit

---
+++ bin/rust-lambda.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { RustLambdaStack } from '../lib/rust-lambda-stack';
+
+const app = new cdk.App();
+new RustLambdaStack(app, 'RustLambdaStack');
